82da5df6768741b1a446691446b959c7cddb09f8
Renaud Chaput <renchap@cocoa-x.com>
2010-12-26 15:03:36 +0100

    Display the full menu without using JSON for the moment

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,13 +1,13 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Picty.helpers do
-  def display_menu
+  def display_menu path = '/'
     dir = Array.new
-    Directory.get_directory('/').children.each do |child|
+    Directory.get_directory(path).children.each do |child|
       dir << { :name => child.name, :path => child.path, :nb_images => child.number_of_images }
     end
-    if dir.length == 0
-      render :haml, '%p No images', :layout => false
+    if dir.length == 0 and path == '/'
+      render :haml, '%p No Albums', :layout => false
     else
       partial 'menu/submenu', :object => dir
     end
