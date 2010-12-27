c998e33e38ec87ed2b51f9841dca816a57d862b1
Renaud Chaput <renchap@cocoa-x.com>
2010-12-27 10:59:25 +0100

    Only convert path to UTF-8 if Ruby 1.9

--- app/controllers/albums.rb
+++ app/controllers/albums.rb
@@ -1,9 +1,13 @@
+require 'natural_sort_kernel'
+
 Picty.controllers :albums do
 
   get :show, :map => '/albums/*path' do
     d = Directory.get_directory('/'+params[:path].join('/'))
     @pictures = d.pictures
-    @path = d.path.force_encoding('utf-8')
+    @path = d.path
+    # Convert the path to UTF-8 if Ruby 1.9
+    @path.force_encoding('utf-8') if @path.respond_to?(:force_encoding)
     render 'albums/show'
   end
 
