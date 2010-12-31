ebee312cefd4e4d4de31084e667d4f274deb9639
Renaud Chaput <renchap@cocoa-x.com>
2010-12-31 16:48:14 +0100

    Add Metadata reading in Picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,28 @@
 class Picture
   attr_reader :filename, :directory
   
+  EXIF_METADATA = {
+    :camera_model => 'model',
+    :exposure => 'ExposureTime',
+    :aperture => 'FNumber',
+    :iso => 'isoSpeedRatings',
+    :focal_lenght => 'FocalLength',
+    :date => 'DateTimeOriginal',
+  }
+
+  IPTC_METADATA = {
+    :title => '2:05',
+    :caption => '2:120',
+  }
+
+  # Create methods to get metadata
+  EXIF_METADATA.merge(IPTC_METADATA).each do |key, value|
+    define_method(key) do
+      self.get_metadata(key)
+    end
+  end
+
+
   class NotFound < Exception
   end
 
@@ -10,7 +32,8 @@ class Picture
     
     # Filename is the last part
     @filename = ar.pop
-    
+    @metadata = nil
+
     begin
       @directory = Directory.get_directory("/#{ar.join('/')}")
     rescue Directory::NotFound => e
@@ -20,6 +43,8 @@ class Picture
     
     # Picture not found if the picture does not exists on disk
     raise Picture::NotFound, "Picture #{path} not found" unless File.file?(self.physical_path)
+
+    # Dynamically create methods for all metadata
   end
   
   def to_param
@@ -44,6 +69,56 @@ class Picture
   
   def url
     "/pictures#{self.path}"
+  end
+
+  def get_metadata(name)
+    unless @metadata
+      @metadata = Hash.new
+      # Get metadata from image using minimagick
+      img = MiniMagick::Image.open(self.physical_path)
+
+      # We request all needed metadatas at one to call ImageMagick only one and speed up the process
+      to_query = Array.new
+      keys = Array.new
+
+      EXIF_METADATA.each do |key, value|
+        keys << key
+        to_query << "%[EXIF:#{value}]"
+      end
+
+      IPTC_METADATA.each do |key, value|
+        keys << key
+        to_query << "%[IPTC:#{value}]"
+      end
+      
+      # Hope nobody uses this separator in metadata
+      data = img[to_query.join('\r||\r')]
+      data.split("\r||\r").each_with_index do |value,index|
+        @metadata[keys[index.to_i]] = value if value
+      end
+
+      # Better format for some metadata
+
+      if @metadata[:aperture]
+        s = @metadata[:aperture].split('/')
+        @metadata[:aperture] = (s[0].to_f/s[1].to_f).to_s
+      end
+
+      [:exposure, :focal_lenght].each do |key|
+        if @metadata[key]
+          s = @metadata[key].split('/')
+          if s[1] == '1'
+            @metadata[key] = s[0]
+          end
+        end
+      end
+
+      @metadata[:date] = DateTime.strptime(@metadata[:date], '%Y:%m:%d %T') if @metadata[:date]
+
+      logger.debug "Metadatas for #{self.path} : "+@metadata.to_s
+      img.destroy!
+    end
+    @metadata[name]
   end
 
   def thumbnail
