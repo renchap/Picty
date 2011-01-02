8b33ae08ba3db0984874799b5e5038a81fe0364f
Renaud Chaput <renchap@cocoa-x.com>
2011-01-02 14:06:37 +0100

    Use image title as page title

--- app/controllers/pictures.rb
+++ app/controllers/pictures.rb
@@ -3,7 +3,8 @@ Picty.controllers :pictures do
   get :show, :map => '/pictures/*picture' do
     @picture = Picture.from_param(params[:picture])
     halt(404, "Picture not found") unless @picture
-    
+   
+    @page_title = @picture.title if @picture.title
     render 'pictures/show'
   end
 
